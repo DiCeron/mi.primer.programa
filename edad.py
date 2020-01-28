7e14bd571fea8f0ca2be6dacdede95c1a39e2f6f
PC <diegoceron899@gmail.com>
2020-01-27 22:02:11 -0600

    añadiendo dato int para poder hacer operaciones

---
+++ edad.py
@@ -0,0 +1,5 @@
+ano_de_nacimiento = int(input("Ingresa tu año de nacimiento: "))
+nombre = input("ingresa tu nombre: ")
+ano = int(input("igresa el año actual: "))
+edad_actual  = ano - ano_de_nacimiento
+print("La edad de {} es {} y nacio en {}".format(nombre, edad_actual, ano_de_nacimiento))
