a52a683ccb398cbdffc6736707c93ec1b5d7780b
PC <diegoceron899@gmail.com>
2020-01-27 22:18:17 -0600

    añadiendo dato int para poder hacer operaciones

---
+++ mayor de tres numeros.py
@@ -0,0 +1,7 @@
+
+primer_numero = int(input("ingresa el primer numero: "))
+segundo_numero = int(input("ingresa el segundo numero: "))
+tercer_numero = int(input("ingrse elñ tercer numero: "))
+
+resultado= max( primer_numero, segundo_numero, tercer_numero)
+print("el resultado es: {}".format(resultado))
