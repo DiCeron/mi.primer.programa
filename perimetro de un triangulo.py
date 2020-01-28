e0d1f7d4c371d9974c7f9c6facff5b93fbb13911
PC <diegoceron899@gmail.com>
2020-01-27 20:03:36 -0600

    añadiendo comando format

--- perimetro de un triangulo.py
+++ perimetro de un triangulo.py
@@ -2,5 +2,4 @@ lado_1 =input("ingresa lado 1: ")
 lado_2 = input("ingresa lado 2: ")
 lado_3 =input("ingresa lado 3: ")
 p= lado_1 + lado_2 + lado_3
-print("el perimetro es : ")
-print(p )
+print("el perimetro es : {}". format (p) )
