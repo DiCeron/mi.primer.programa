1de3f16d369428a69abcd9673856cfed4022e2b0
PC <diegoceron899@gmail.com>
2020-01-30 22:21:40 -0600

    Conversion Fahrenheit a Celsius

---
+++ fahrenheit_a_celsius.py
@@ -0,0 +1,3 @@
+fahrenheit = int(input("ingresa los grados a convertir:  "))
+celsius = (fahrenheit - 32)/1.8
+print("EL resultado de la conversion de {} fahrenheit a celsius es : {}".format(fahrenheit, celsius))
