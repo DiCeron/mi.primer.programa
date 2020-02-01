2a14982a2cd60d61c0c64fc0c2cd8ac38acc5269
PC <diegoceron899@gmail.com>
2020-01-31 22:10:59 -0600

    agregando .upper para mostrar las mayusculas introducidas

--- Mayusculas.py
+++ Mayusculas.py
@@ -1,12 +1,13 @@
 texto = input("introduce un texto: ")
 
-
 mayusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
+mayus = []
 n_mayusculas = 0
 
-for mayus in texto :
-    if mayus in mayusculas:
+for mayu in texto:
+    if mayu in mayusculas:
         n_mayusculas += 1
+        mayus.append(mayu)
 
 print("Mayusculas= {}".format(n_mayusculas))
+print(mayus)
